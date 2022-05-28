d0c0efede620100eca2f4d888fc7f903f8e66c51
Cherif-ahmed <chermed.ac@gmail.com>
2022-05-28 14:26:44 +0100

    Added report summary to AdminOwner/Admin Dashboard

---
+++ src/js/adminReport.js
@@ -0,0 +1,24 @@
+async function loadReport(){
+    contract = await $.getJSON('../Auth.json');
+    contractAddress = localStorage.getItem('contractAddress')
+
+    web3 = new Web3(window.ethereum);
+    Authcontract = new web3.eth.Contract(contract.abi, contractAddress);
+    accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+
+    AdminCount =parseInt( await Authcontract.methods.AdminCount.call().call())
+    patientCount =parseInt( await Authcontract.methods.patientCount.call().call())
+    doctorCount =parseInt( await Authcontract.methods.doctorCount.call().call())
+
+    if(window.location.pathname.split("/")[1]=="AdminOwner"){
+        document.getElementById("AdminNum").innerHTML= AdminCount
+    }
+    document.getElementById("PatientNum").innerHTML= patientCount
+    document.getElementById("DoctorNum").innerHTML= doctorCount
+
+}
+
+
+$( ()=>{
+    $(window).on("Load", loadReport())
+  })
